1f5e1ba090148800f1aabd45451ab47bed9387be
Aleksandr Khan <manchig1990@gmail.com>
2024-02-13 18:41:47 +1000

    add Array Library

---
+++ Example004_ArrayLibrary/Program.cs
@@ -0,0 +1,49 @@
+﻿void FillArray(int[] collection) //Метод заполнения массива
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) //Метод печати на экран
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        System.Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find) //Метон нихождения индекса
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; //создаем массив из 10ти эллементов
+
+FillArray(array);
+array[4] = 4;
+array[7] = 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 44);
+System.Console.WriteLine(pos);
