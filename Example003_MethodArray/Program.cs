8f43e8e31e1926f95fcca6a16d2361d6264fff88
Aleksandr Khan <manchig1990@gmail.com>
2024-02-13 18:17:05 +1000

    add Method Array

---
+++ Example003_MethodArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// ********************************************************
+// Находим максимальное число и вывводим index и останавливаем программу
+
+
+int[] array = { 81, 28, 30, 256, 94, 55, 56, 47, 256};
+
+int n = array.Length;
+int find = 256;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break; 
+    }
+    index++;
+}
